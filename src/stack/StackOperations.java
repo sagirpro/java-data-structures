ca1aa4c05569d4c180db225139a7b0a904a41de8
sagirpro <sagir.pro@gmail.com>
2014-08-08 00:09:31 +0530

    stack operation

--- src/stack/StackOperations.java
+++ src/stack/StackOperations.java
@@ -1,53 +1,42 @@
 package stack;
 
-
+import java.util.Stack;
 
 
 public class StackOperations {
-	private final int ARRAY_SIZE = 9;
-	private String[] movies = new String[ARRAY_SIZE]; // Hold movie titles
-	private int top = 0;
+	public static void main(String args[]) {
+		
+		
+		// creating stack
+		Stack<Integer> st = new Stack<Integer>();
 
-	/**
-	 * Constructor
-	 * 
-	 * @param moviesIn
-	 * @return 
-	 * @return 
-	 */
+		// populating stack
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		st.push(5);
+		st.push(6);
+		
+		//check last element of the stack without removing it
+		 System.out.println("Last Element in the stack: " + st.peek());
 
-	public void Stack(String[] moviesIn) {
-		movies = moviesIn;
+		// Pop operation
+		System.out.println("Removed object is: " + st.pop());
+		
+		//check last element of the stack without removing it
+		 System.out.println("Last Element in the stack: " + st.peek());
+		 
+		// Push operation
+		System.out.println("Elements after remove: " + st);
+		
+		// Search the position of the object
+		System.out.println("The position of the object : 2 is (from top) =  " + st.search(2));
+		
+		// to check weather the stack is empty or not
+		System.out.println("Empty ?  =  " + st.empty());
+		
+		
 	}
 
-	/**
-	 * Test for full stack
-	 * @return 
-	 */
-
-	public void push(String moviesIn) {
-		if (top >= movies.length)
-			System.out.println("ERROR: Stack is full");
-
-		top++;
-		movies[top] = moviesIn;
-	}
-
-	/**
-	 * Test for empty stack
-	 */
-
-	public String pop() {
-		if (top == 0) {
-			System.out.println("ERROR: Stack is empty");
-			return " ";
-		}
-
-		top--;
-		return movies[top];
-	}
-
-	public void peek() {
-		// ???
-	}
 }
