ee9428b429c6ff8f2a7e98a845630c1cfe8da642
sagirpro <sagir.pro@gmail.com>
2014-08-15 01:21:24 +0530

    heap update

--- src/Heap/HeapSort.java
+++ src/Heap/HeapSort.java
@@ -6,6 +6,21 @@
 
 package Heap;
 
+/**
+ * The HeapSort program simply take a series of input and sort
+ * them using Heap Sort algorithm. The Heap follows a data
+ * structure that,  "If A is a parent node of B then the key
+ * of node A is ordered with respect to the key of node B 
+ * with the same ordering applying across the heap. Either 
+ * the keys of parent nodes are always greater than or equal 
+ * to those of the children and the highest key is in the root 
+ * node (this kind of heap is called max heap)". We will use 
+ * Maxheap to sort the series of number.
+ * 
+ * @author Sagir <sagir.pro@gmail.com>
+ * @since 2014-08-09
+ */
+
 public class HeapSort 
 { 
     private static int[] a; 
